1ff57f09b9e461a26e0abbbca5cae8410cbbd6d1
Alper <asunar.git@gmail.com>
2013-04-21 22:21:51 -0500

    Added GetLinesToProcess method and refactored tests

--- Class1.cs
+++ Class1.cs
@@ -56,5 +56,55 @@ namespace CodeKata_DataMunging
            Assert.AreEqual(minGoalDifference.Team, "Aston_Villa");
            Assert.AreEqual(minGoalDifference.GoalDifference, 1);
         }
+
+        private IEnumerable<string> GetLinesToProcess(string fileName, int numberOfLinesToSkip, Func<string, bool> isValidLine)
+        {
+            var lines =
+                File.ReadAllLines(Environment.CurrentDirectory + @"\" + fileName)
+                    .Skip(numberOfLinesToSkip)
+                    .Where(isValidLine);
+            return lines;
+        }
+
+        [Test]
+        public void FindMinGoalDifference_DRY()
+        {
+            Func<string, bool> isValidLine = x => char.IsNumber(x.TrimStart().ToCharArray()[0]);
+            var lines = GetLinesToProcess("football.dat", 5, isValidLine);
+
+            var minGoalDifference = lines.Select(
+                x => new
+                {
+                    Team = x.Substring(7, 16).Trim(),
+                    GoalDifference =
+                    Math.Abs(
+                    Convert.ToInt32(x.Substring(43, 4).Trim()) - Convert.ToInt32(x.Substring(50, 6).Trim()))
+                }
+                ).OrderBy(x => x.GoalDifference).First();
+
+            Assert.AreEqual(minGoalDifference.Team, "Aston_Villa");
+            Assert.AreEqual(minGoalDifference.GoalDifference, 1);
+        }
+
+        [Test]
+        public void FindMaxSpread_DRY()
+        {
+            Func<string, bool> isValidLine = x => char.IsNumber(x.TrimStart().ToCharArray()[0]);
+            var lines = GetLinesToProcess("weather.dat", 8, isValidLine);
+
+            var dayMaxSpread = lines.Select(
+                                x => new
+                                {
+                                    Day = x.Substring(1, 3).Trim(),
+                                    Spread = Convert.ToDecimal(x.Substring(5, 3).Trim()) - Convert.ToDecimal(x.Substring(11, 3).Trim())
+                                }
+                                 ).OrderByDescending(x => x.Spread).First();
+
+
+            Assert.AreEqual(dayMaxSpread.Day, "9");
+            Assert.AreEqual(dayMaxSpread.Spread, 54);
+
+        }
+
     }
 }
